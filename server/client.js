a686f97cab4532bb3554df446bef73043902bc0f
rafaa1999 <raferhouma10@gmail.com>
2022-12-16 17:09:19 +0000

    Client-side validation for the fields with * as required and for the email field, topic title should be max 100 length

--- server/client.js
+++ server/client.js
@@ -102,6 +102,46 @@ function debounce(fn, time){
 
 }
 
+// Validation
+function checkValidity(formData){
+  
+  const name = formData.get('author_name');
+  const email = formData.get('author_email');
+  const topic = formData.get('topic_title');
+  const topicDetails = formData.get('topic_details');
+
+  if(!name){
+    document.querySelector('[name=author_name]').classList.add('is-invalid')
+  }
+
+  const emailPattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  if(!email || !emailPattern.test(email) ){
+    document.querySelector('[name=author_email]').classList.add('is-invalid')
+  }
+  
+  if(!topic || topic.length > 30){
+    document.querySelector('[name=topic_title]').classList.add('is-invalid')
+  }
+
+  if(!topicDetails){
+    document.querySelector('[name=topic_details]').classList.add('is-invalid')
+  }
+
+  const allInvalidElms = document.getElementById('formVideoRequest').querySelectorAll('.is-invalid')
+
+  if(allInvalidElms.length){
+    allInvalidElms.forEach(elm=>{
+      elm.addEventListener('input', function(){
+        this.classList.remove('is-invalid')
+      })
+    })
+    return false;
+  }
+
+  return true;
+
+}
+
 // The entry point of js
 document.addEventListener('DOMContentLoaded', function(){
 
@@ -143,7 +183,12 @@ document.addEventListener('DOMContentLoaded', function(){
   // Submit video
   formVidReqElm.addEventListener('submit',(e)=>{
       e.preventDefault(); 
+
       const formData = new FormData(formVidReqElm);
+
+      const isValid = checkValidity(formData);
+
+      if(!isValid) return;
 
       fetch('http://localhost:7777/video-request',{
         method:"POST",
